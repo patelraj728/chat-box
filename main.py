a729a7f2ef7e40a7adf6f0b3d97166ba6c93d432
patelraj728 <patelraj29112000@gmail.com>
2026-04-16 15:09:25 +0530

    fix socket for render

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask,redirect,url_for,render_template,request,session
 from flask_socketio import SocketIO,rooms,join_room,leave_room,send
 import random
